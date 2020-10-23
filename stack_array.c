3a95117fdb3c5ab21095facec72c20b3d47b2151
Sharad Maddheshiya <59807761+sharad9@users.noreply.github.com>
2020-10-24 00:47:29 +0530

    Update stack_array.c

--- stack_array.c
+++ stack_array.c
@@ -11,7 +11,7 @@ struct Stack* createStack(unsigned capacity) {
     struct Stack *stack = (struct Stack*)malloc(sizeof(struct Stack));
     stack->capacity = capacity;
     stack->top = -1;
-    stack-> array = (int*)malloc(sizeof(int));
+    stack-> array = (int*)malloc(stack->capacity *sizeof(int));
     return stack;
 }
 
