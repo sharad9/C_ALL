e6347b59c6fafbf51f039f00cdd1cbd731196baf
Sharad Maddheshiya <59807761+sharad9@users.noreply.github.com>
2020-10-24 19:05:16 +0530

    Update CircularQueueLinkedList.c

--- CircularQueueLinkedList.c
+++ CircularQueueLinkedList.c
@@ -26,15 +26,18 @@ void enQueue(struct Queue* q, int data){
     struct QNode* newNode = createNode(data);
     if(q->front == NULL){
         q->front = newNode;   
-    }
+    }else{
         q->rear->next = newNode;
-        q->rear = newNode;
-        q->rear->next = q->front;
+    }
+        
+    q->rear = newNode;
+    q->rear->next = q->front;
     
 }
 int deQueue(struct Queue* q){
     if(q->front == NULL)
         return INT_MIN;
+        
     if(q->front == q->rear){
         int data = q->front->data;
         q->front = q->rear = NULL;
@@ -43,34 +46,42 @@ int deQueue(struct Queue* q){
     }else{  
         int data = q->front->data;
         q->front = q->front->next;
-        q->rear = q->front;
+        q->rear->next= q->front;
         return data;
     }
 }
 
-void DisplayCircularQ(struct Queue* q){
+void displayQueue(struct Queue* q){
     struct QNode* temp = q->front;
     printf("\nElements in Circular Queue are: "); 
-    while(temp->next != q->rear){
+    while(temp->next != q->front){
          printf("%d ", temp->data);
          temp = temp->next;
     }
-    printf("%d ", temp->data);
+   printf("%d ", temp->data);
     
 }
 
 int main(){
     struct Queue* q = createQueue();
-    enQueue(q, 10); 
-    /*
+    
+   
+  
+    
+    enQueue(q, 14); 
+    enQueue(q, 22); 
+    enQueue(q, 6); 
+  
+    
+    displayQueue(q); 
+  
+   
+    printf("\nDeleted value = %d", deQueue(q)); 
+    printf("\nDeleted value = %d", deQueue(q)); 
+  
+  
+    enQueue(q, 9); 
     enQueue(q, 20); 
-    deQueue(q); 
-    deQueue(q); 
-    enQueue(q, 30); 
-    enQueue(q, 40); 
-    enQueue(q, 50); 
-    deQueue(q); 
-    printf("Queue Front : %d \n", q->front->data);
-    printf("Queue Rear : %d \n", q->rear->data);
-    */
+    displayQueue(q); 
+   
 }
