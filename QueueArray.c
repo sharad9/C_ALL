bcac82a743dbb82eb070205c141e1f0d5a531b33
Sharad Maddheshiya <59807761+sharad9@users.noreply.github.com>
2020-10-24 22:58:44 +0530

    Update QueueArray.c

--- QueueArray.c
+++ QueueArray.c
@@ -10,7 +10,7 @@ struct Queue{
 struct Queue* createQueue(unsigned capacity){
     struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
     queue->capacity = capacity;
-    
+    queue->front = 0;
     queue->rear = capacity-1;
     queue->array = (int*)malloc(queue->capacity*sizeof(int)); 
     return queue;
