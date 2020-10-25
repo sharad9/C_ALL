ec6715aadd664cee78cffe79bf0b41fa3b74535f
Sharad Maddheshiya <59807761+sharad9@users.noreply.github.com>
2020-10-25 21:41:41 +0530

    Update QueueArray.c

--- QueueArray.c
+++ QueueArray.c
@@ -4,7 +4,7 @@
 
 struct Queue{
     unsigned capacity;
-    int front,rear;
+    int front,rear,size;
     int *array;
 };
 struct Queue* createQueue(unsigned capacity){
@@ -16,23 +16,23 @@ struct Queue* createQueue(unsigned capacity){
     return queue;
 }
 int isFull(struct Queue* queue){
-    return queue->rear == queue->capacity;
+    return queue->size == queue->capacity;
 }
 int isEmpty(struct Queue* queue){
-    return queue->front == -1;
+    return queue->size == 0;
 }
 void enqueue(struct Queue* queue, int data){
     if(isFull(queue)){printf("Queue is FULL\n");return;}
     queue->rear = (queue->rear+1)%queue->capacity;
     queue->array[queue->rear] = data;
-    
+    queue->size = queue->size + 1;
     printf("%d enqueued to queue\n", data);
 }
 int dequeue(struct Queue* queue){
     if(isEmpty(queue)){printf("Queue is EMPTY\n");return INT_MIN;}
     int data = queue->array[queue->front];
     queue->front = (queue->front+1)%queue->capacity;
-    
+    queue->size = queue->size - 1;
     return data;
 }
 int Front(struct Queue* queue){
@@ -48,6 +48,9 @@ int main(){
     enqueue(queue,1);
     enqueue(queue,2);
     enqueue(queue,3);
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    printf("%d dequeued from queue\n\n",dequeue(queue));
     printf("%d dequeued from queue\n\n",dequeue(queue));
     printf("Front item is %d\n", Front(queue)); 
     printf("Rear item is %d\n", Rear(queue)); 
