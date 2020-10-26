aae53609daf57241027ea9869ef99d9a8b99d6f5
Sharad Maddheshiya <59807761+sharad9@users.noreply.github.com>
2020-10-26 12:11:24 +0530

    Update Circular Queue Array.c

--- Circular Queue Array.c
+++ Circular Queue Array.c
@@ -16,26 +16,23 @@ struct Queue* createQueue(unsigned capacity){
     return queue;
 }
 int isFull(struct Queue* queue){
-    return queue->size == 0;
+    return  queue->size == queue->capacity;
 }
 int isEmpty(struct Queue* queue){
-    return queue->size<0;
+    return queue->size == 0;
 }
 void enqueue(struct Queue* queue, int data){
+    if(isFull(queue)){printf("Queue is FULL\n");return INT_MIN;}
     queue->size = queue->size<queue->capacity? queue->size+1: queue->size;
     queue->rear = (queue->rear+1)%queue->capacity;
     queue->array[queue->rear] = data;
-   
     printf("%d enqueued to queue\n", data);
 }
 int dequeue(struct Queue* queue){
-    queue->size =  queue->size-1;
     if(isEmpty(queue)){printf("Queue is EMPTY\n");return INT_MIN;}
-    
+    queue->size = queue->size>0? queue->size-1: queue->size;
     int data = queue->array[queue->front];
     queue->front = (queue->front+1)%queue->capacity;
-    
-   
     return data;
 }
 int Front(struct Queue* queue){
@@ -46,16 +43,42 @@ int Rear(struct Queue* queue){
     if(isEmpty(queue)){printf("Queue is EMPTY\n");return INT_MIN;}
     return queue->array[queue->rear];
 }
+void displayAll(struct Queue* queue){
+    printf("Elements in the queue :\n");
+    if(isEmpty(queue)){printf("Queue is EMPTY\n");return INT_MIN;}
+    
+    for(int i = 0; i<queue->size;i++){
+        printf("   _ %d  \n",queue->array[i]);
+    }
+    printf("");
+        
+}
 int main(){
-    struct Queue* queue = createQueue(4);
+    struct Queue* queue = createQueue(3);
+    enqueue(queue,1);
+    enqueue(queue,2);
+    printf("Front item is %d\n", Front(queue)); 
+    printf("Rear item is %d\n", Rear(queue)); 
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    printf("Front item is %d\n", Front(queue)); 
+    printf("Rear item is %d\n", Rear(queue)); 
+    enqueue(queue,3);
+    enqueue(queue,4);
+    printf("Front item is %d\n", Front(queue)); 
+    printf("Rear item is %d\n", Rear(queue)); 
+    displayAll( queue);
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    printf("%d dequeued from queue\n\n",dequeue(queue));
+    displayAll( queue);
     enqueue(queue,1);
     enqueue(queue,2);
     enqueue(queue,3);
-     enqueue(queue,4);
-    printf("%d dequeued from queue\n\n",dequeue(queue));
-    printf("%d dequeued from queue\n\n",dequeue(queue));
-   
-     printf("%d dequeued from queue\n\n",dequeue(queue));
+    enqueue(queue,4);
+    displayAll(queue);
     printf("Front item is %d\n", Front(queue)); 
     printf("Rear item is %d\n", Rear(queue)); 
   
